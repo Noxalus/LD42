109fe928d36957ab61d3bb85e7deb19d9ff8bfce
Noxalus <noxalus@gmail.com>
2018-08-11 15:53:15 +0200

    Add the pickup system.

--- LD42/Assets/Scripts/PlayerArmsManager.cs
+++ LD42/Assets/Scripts/PlayerArmsManager.cs
@@ -5,8 +5,15 @@ public class PlayerArmsManager : MonoBehaviour
     public PackageDetector PackageDetector;
     public Rigidbody2D LeftArmRigidBody;
     public Rigidbody2D RightArmRigidBody;
+    public Transform LeftHand;
+    public Transform RightHand;
 
     private GameObject _nearestPackage;
+
+    // Picking up
+    private bool _pickingUp;
+    private GameObject _pickedObject;
+    private TargetJoint2D _pickupTargetJoint;
 
     public void Start()
     {
@@ -15,11 +22,32 @@ public class PlayerArmsManager : MonoBehaviour
 
     public void SetNearestPackage(GameObject package)
     {
+        if (_pickingUp)
+            return;
+
         _nearestPackage = package;
     }
 
     public void Update()
     {
+        // Pickup nearest package
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PickupPackage(_nearestPackage);
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            ReleasePackage();
+        }
+
+        if (_pickingUp)
+        {
+            if (_pickupTargetJoint)
+            {
+                _pickupTargetJoint.target = GetPickupPosition();
+            }
+        }
+
         if (_nearestPackage)
         {
             var leftArmDirectionX = LeftArmRigidBody.position.x - _nearestPackage.transform.position.x;
@@ -32,5 +60,35 @@ public class PlayerArmsManager : MonoBehaviour
             var rightArmAngle = (-Mathf.Atan2(rightArmDirectionX, rightArmDirectionY) * Mathf.Rad2Deg) + 180f;
             RightArmRigidBody.rotation = rightArmAngle;
         }
+    }
+
+    public void PickupPackage(GameObject package)
+    {
+        _pickupTargetJoint = package.GetComponent<TargetJoint2D>();
+
+        if (!_pickupTargetJoint)
+            _pickupTargetJoint = package.AddComponent<TargetJoint2D>();
+
+        _pickupTargetJoint.target = GetPickupPosition();
+
+        _pickingUp = true;
+        _pickedObject = package;
+
+        // Disable collision box
+        package.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public Vector2 GetPickupPosition()
+    {
+        return LeftHand.position + (RightHand.position - LeftHand.position) / 2;
+    }
+
+    public void ReleasePackage()
+    {
+        // Disable collision box
+        _pickedObject.GetComponent<BoxCollider2D>().enabled = true;
+
+        Destroy(_pickupTargetJoint);
+        _pickingUp = false;
     }
 }
