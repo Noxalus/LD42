52d4e52c51fdd7c070c6996eb0ab18eba29bf13d
Noxalus <noxalus@gmail.com>
2018-08-11 16:33:39 +0200

    Add simple pipe spawner.

---
+++ LD42/Assets/Scripts/PackageSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageSpawner : MonoBehaviour
+{
+    public List<GameObject> PackagePrefabs;
+    public float StartDelay;
+
+    private bool _disabled;
+
+    public void Start()
+    {
+        _disabled = false;
+        StartCoroutine(SpawnPackage());
+    }
+
+    private IEnumerator SpawnPackage()
+    {
+        yield return new WaitForSeconds(StartDelay);
+
+        while (!_disabled)
+        {
+            Instantiate<GameObject>(PackagePrefabs[Random.Range(0, PackagePrefabs.Count)], transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
