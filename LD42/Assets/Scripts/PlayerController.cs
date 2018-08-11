cb7fec088f47e7c9c3d848c6616bff97eb925f51
Noxalus <noxalus@gmail.com>
2018-08-11 12:59:32 +0200

    Add a script to control the player + some 2D physics.

---
+++ LD42/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D RigidBody;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        RigidBody.AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+}
